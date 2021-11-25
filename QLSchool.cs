34512636f63494746f1bc0033d348c0e0ca0dad9
YamiDP <20110546@student.hcmute.edu.vn>
2021-11-25 14:57:33 +0700

    Upload source code 2

--- QLSchool.cs
+++ QLSchool.cs
@@ -11,7 +11,7 @@ namespace SchoolManager
         {
             int chon;
             do
-            {   //Hello Phuong
+            {   
                 Console.WriteLine("------------QUAN LY TRUONG HOC------------");
                 Console.WriteLine("1.Quan ly giang vien");
                 Console.WriteLine("2.Quan ly sinh vien");
