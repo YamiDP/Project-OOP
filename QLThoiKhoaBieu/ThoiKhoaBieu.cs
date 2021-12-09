f674d51cfb8185a18ca69dbab8e4a59d90836398
YamiDP <20110546@student.hcmute.edu.vn>
2021-12-09 14:37:30 +0700

    Thoi Khoa bieu

---
+++ QLThoiKhoaBieu/ThoiKhoaBieu.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SchoolManager.QLMonHoc;
+namespace SchoolManager.QLThoiKhoaBieu
+{
+    public class TKB
+    {
+        private int namhoc;
+        private string hocky;
+        private int tuan;
+        private string diachiphonghoc;
+        private MonHoc thongtinmonhoc;
+        
+    }
+}
