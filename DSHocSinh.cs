315f930f5d25b49442dd1669c5306a70d791bd1c
YamiDP <20110546@student.hcmute.edu.vn>
2021-12-13 22:21:48 +0700

    Cap nhat class HocSinh va DSHocSinh

--- DSHocSinh.cs
+++ DSHocSinh.cs
@@ -7,8 +7,22 @@ namespace SchoolManager
 {
     public class DSHocSinh
     {
+        
         //khoi tao danh sach hoc sinh
         public static List<HocSinh> lisths = new List<HocSinh>();
+        //tao du lieu danh sach hoc sinh co ban
+        public static void base_list()
+        {
+            HocSinh hs1 = new HocSinh("125","Phuong","02/02/2002","Nam","Binh Phuoc","032342342",25.5,"A1","CNTT",1,4,"Dai Hoc");
+            lisths.Add(hs1);
+            HocSinh hs2 = new HocSinh("124","Nam","12/02/2002","Nam","Da Lat","0321242",23.5,"A1","CNTT",1,4,"Dai Hoc");
+            lisths.Add(hs2);
+            HocSinh hs3 = new HocSinh("121","Tuan","02/04/2002","Nam","Binh Dinh","0343534342",24,"A","CNTT",1,4,"Dai Hoc");
+            lisths.Add(hs3);
+            HocSinh hs4 = new HocSinh("122","Hung","11/02/2002","Nam","Vung Tau","032323642",26.5,"D","May",1,4,"Dai Hoc");
+            lisths.Add(hs4);
+        }
+        //ham kiem tra ma hoc sinh trong danh sach
         public static HocSinh checkHS(string Sbd)
         {
             foreach (HocSinh hs in lisths)
@@ -19,12 +33,13 @@ namespace SchoolManager
             return null;
         }
         //them hoc sinh vao danh sach
-        public static void inputHS()
+        public static void addHS()
         {
             HocSinh hs = new HocSinh();
             hs.input();
             lisths.Add(hs);
             Console.WriteLine("Them hoc sinh thanh cong!");
+            Console.WriteLine();
         }
         //in danh sach hoc sinh        
         public static void printHS()
@@ -40,27 +55,48 @@ namespace SchoolManager
             {
                 hs.print();
             }
+            Console.WriteLine();
         }
-        //sap xep danh sach hoc sinh
-        public static void sort()
+        //sap xep danh sach hoc sinh theo ten
+        public static void sort_name()
         {
             if(lisths.Count == 0)
             {
                 Console.WriteLine("Khong co hoc sinh nao trong danh sach!");
                 return;
             }
-            var DSHS = lisths.OrderByDescending(hs=>hs.TEN);
+            var DSHS = lisths.OrderBy(hs=>hs.TEN);
+            Console.WriteLine("Danh sach sau khi sap xep theo ten:");
             Console.WriteLine("{0, -7} {1, -20} {2, -10} {3, -10} {4, -15} {5,-15} {6,-10} {7,-9} {8,-10} {9,-10} {10,-12} {11,-10}",
                              "MaHS", "TenHS", "Ngay Sinh", "Gioi Tinh", "Dia Chi", "SĐT", "Diem Thi", "Khoi thi", "Nganh", "Dien UT", "So luong NV", "Phuong thuc xet tuyen");
-            foreach (HocSinh hs in lisths)
+            foreach (HocSinh hs in DSHS)
             {
                 hs.print();
             }
+            Console.WriteLine();
         }
+        //sap xep hoc sinh theo ma so
+        public static void sort_MA()
+        {
+            if(lisths.Count == 0)
+            {
+                Console.WriteLine("Khong co hoc sinh nao trong danh sach!");
+                return;
+            }
+            var DSHS = lisths.OrderBy(hs=>hs.MA);
+            Console.WriteLine("Danh sach sau khi sap xep theo ma hoc sinh:");
+            Console.WriteLine("{0, -7} {1, -20} {2, -10} {3, -10} {4, -15} {5,-15} {6,-10} {7,-9} {8,-10} {9,-10} {10,-12} {11,-10}",
+                             "MaHS", "TenHS", "Ngay Sinh", "Gioi Tinh", "Dia Chi", "SĐT", "Diem Thi", "Khoi thi", "Nganh", "Dien UT", "So luong NV", "Phuong thuc xet tuyen");
+            foreach (HocSinh hs in DSHS)
+            {
+                hs.print();
+            }
+            Console.WriteLine();
+        }  
         //tim kiem hoc sinh trong danh sach
         public static void searchHS()
         {
-            Console.Write("Nhap ma hoc sinh: ");
+            Console.Write("Nhap ma hoc sinh muon tim kiem: ");
             string Mahs = Console.ReadLine();
             HocSinh hs = checkHS(Mahs);
             if (lisths.Count == 0 || hs == null)
@@ -71,6 +107,7 @@ namespace SchoolManager
             Console.WriteLine("{0, -7} {1, -20} {2, -10} {3, -10} {4, -15} {5,-15} {6,-10} {7,-9} {8,-10} {9,-10} {10,-12} {11,-10}",
                              "MaHS", "TenHS", "Ngay Sinh", "Gioi Tinh", "Dia Chi", "SĐT", "Diem Thi", "Khoi thi", "Nganh", "Dien UT", "So luong NV", "Phuong thuc xet tuyen");
             hs.print();
+            Console.WriteLine();
         }
         //xoa hoc sinh ra khoi danh sach
         public static void deleteHS()
@@ -85,8 +122,10 @@ namespace SchoolManager
             }
             lisths.Remove(hs);
             Console.WriteLine("Da xoa thanh cong!");
+            Console.WriteLine();
 
         }
+        //ham tra ve so luong hoc sinh trong danh sach
         public static int SLHS() 
         {
             return lisths.Count;
--- HocSinh.cs
+++ HocSinh.cs
@@ -42,6 +42,7 @@ namespace SchoolManager
             set { phuongthucxettuyen = value; }
             get { return phuongthucxettuyen; }
         }
+        //Ham nhap thong tin hoc sinh
         public override void input()
         {
             base.input();
@@ -58,14 +59,14 @@ namespace SchoolManager
             Console.Write("Phuong thuc xet tuyen: ");
             Phuongthucxettuyen = Convert.ToString(Console.ReadLine());
         }
-  
+        //Ham in thong tin mot hoc sinh
         public override void print()
         {
             Console.WriteLine("{0, -7} {1, -20} {2, -10} {3, -10} {4, -15} {5,-15} {6,-10} {7,-9} {8,-10} {9,-10} {10,-12} {11,-10}",
                                    MA, TEN, NGSINH, GTINH, DIACHI, SDT, Diemthi, Khoithi, Nganh, DienUT, SoluongNV, Phuongthucxettuyen);
         }
 
-
+        //Cac ham khoi tao
         public HocSinh(string ma, string ten, string ngsinh, string gtinh, string diachi, string sdt,
         double diemthi, string khoithi, string nganh, int dienUT, int soluongNV, string phuongthucxettuyen)
             : base(ma, ten, ngsinh, gtinh, diachi, sdt)
@@ -91,157 +92,10 @@ namespace SchoolManager
             soluongNV = 0;
             phuongthucxettuyen = "N/A";
         }
+        //Ham huy
         ~HocSinh()
         {
             
         }
     }
 }
-
-/* namespace SchoolManager.QLTuyenSinh
-{
-    public class managerHocSinh : HocSinh
-    {
-        public static List<HocSinh> lisths = new List<HocSinh>();
-        public static HocSinh checkHS(string Sbd)
-        {
-            foreach (HocSinh hs in lisths)
-            {
-                if (String.Compare(hs.MA, Sbd, true) == 0)
-                    return hs;
-            }
-            return null;
-        }
-        public void inputHS()
-        {
-            HocSinh hs;
-            do
-            {
-                Console.Write("Ma hoc sinh:");
-                MA = Convert.ToString(Console.ReadLine());
-                hs = checkHS(MA);
-                if (hs != null)
-                {
-                    Console.WriteLine("Ma hoc sinh da ton tai!");
-                    Console.WriteLine("Vui long nhap lai!!!");
-                }
-            } while (hs != null);
-            base.input();
-            Console.Write("Diem thi: ");
-            Diemthi = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Khoi thi: ");
-            Khoithi = Convert.ToString(Console.ReadLine());
-            Console.Write("Nganh: ");
-            Nganh = Convert.ToString(Console.ReadLine());
-            Console.Write("Dien uu tien: ");
-            DienUT = Convert.ToInt16(Console.ReadLine());
-            Console.Write("So luong nguyen vong: ");
-            SoluongNV = Convert.ToInt16(Console.ReadLine());
-            Console.Write("Phuong thuc xet tuyen: ");
-            Phuongthucxettuyen = Convert.ToString(Console.ReadLine());
-            hs = new HocSinh(MA, TEN, NGSINH, GTINH, DIACHI, SDT, Diemthi, Khoithi, Nganh, DienUT, SoluongNV, Phuongthucxettuyen);
-            lisths.Add(hs);
-
-            Console.WriteLine("----------------------------------------------");
-            Console.WriteLine("Dang ki hoc sinh thanh cong!\n");
-        }
-        public override void print()
-        {
-            if (lisths.Count == 0)
-            {
-                Console.WriteLine("Khong co hoc sinh nao trong danh sach dang ki!");
-            }
-            else
-            {
-                Console.WriteLine("{0, -7} {1, -20} {2, -10} {3, -10} {4, -15} {5,-15} {6,-10} {7,-9} {8,-10} {9,-10} {10,-12} {11,-10}",
-                             "MaHS", "TenHS", "Ngay Sinh", "Gioi Tinh", "Dia Chi", "SĐT", "Diem Thi", "Khoi thi", "Nganh", "Dien UT", "So luong NV", "Phuong thuc xet tuyen");
-                foreach (HocSinh hs in lisths)
-                {
-                    Console.WriteLine("{0, -7} {1, -20} {2, -10} {3, -10} {4, -15} {5,-15} {6,-10} {7,-9} {8,-10} {9,-10} {10,-12} {11,-10}",
-                                   hs.MA, hs.TEN, hs.NGSINH, hs.GTINH, hs.DIACHI, hs.SDT, hs.Diemthi, hs.Khoithi, hs.Nganh, hs.DienUT, hs.SoluongNV, hs.Phuongthucxettuyen);
-                }
-            }
-
-            Console.WriteLine();
-        }
-        public override void search()
-        {
-            if (lisths.Count == 0)
-            {
-                Console.WriteLine("Khong co hoc sinh nao trong danh sach!");
-            }
-            else
-            {
-                Console.Write("Nhap ma hoc sinh: ");
-                string MaHS = Console.ReadLine();
-
-                foreach (HocSinh hs in lisths)
-                {
-                    if (String.Compare(hs.MA, MaHS, false) == 0)
-                    {
-                        Console.WriteLine("Thong tin hoc sinh can tim:");
-                        Console.WriteLine("{0, -7} {1, -20} {2, -10} {3, -10} {4, -15} {5,-15} {6,-10} {7,-9} {8,-10} {9,-10} {10,-12} {11,-10}",
-                             "MaHS", "TenHS", "Ngay Sinh", "Gioi Tinh", "Dia Chi", "SĐT", "Diem Thi", "Khoi thi", "Nganh", "Dien UT", "So luong NV", "Phuong thuc xet tuyen");
-                        Console.WriteLine("{0, -7} {1, -20} {2, -10} {3, -10} {4, -15} {5,-15} {6,-10} {7,-9} {8,-10} {9,-10} {10,-12} {11,-10}",
-                                  hs.MA, hs.TEN, hs.NGSINH, hs.GTINH, hs.DIACHI, hs.SDT, hs.Diemthi, hs.Khoithi, hs.Nganh, hs.DienUT, hs.SoluongNV, hs.Phuongthucxettuyen);
-                    }
-                }
-
-            }
-        }
-        public void deleteHS()
-        {
-            Console.Write("Nhap ma hoc sinh: ");
-            string MaHS = Console.ReadLine();
-            HocSinh hs1 = checkHS(MaHS);
-            if (hs1 == null)
-            {
-                Console.WriteLine("Khong tim thay hoc sinh!");
-            }
-            else
-            {
-                lisths.Remove(hs1);
-                Console.WriteLine("Da xoa hoc sinh thanh cong!");
-            }
-            Console.WriteLine("\n\n");
-        }
-
-        public delegate bool DelSort(HocSinh hs1, HocSinh hs2);
-        public static bool CompareRise(HocSinh hs1, HocSinh hs2)
-        {
-            if (String.Compare(hs1.TEN, hs2.TEN, false) > 0)
-                return false;
-            return true;
-        }
-        public override void sort()
-        {
-            DelSort d = new DelSort(CompareRise);
-            if (lisths.Count == 0)
-            {
-                Console.WriteLine("Danh sach rong! \n\n");
-            }
-            else
-            {
-                List<HocSinh> lisths1 = new List<HocSinh>(lisths);
-                HocSinh hs1;
-                for (int i = 0; i < lisths1.Count - 1; i++)
-                    for (int j = i + 1; j < lisths1.Count; j++)
-                    {
-                        if (!d(lisths1[i], lisths1[j]))
-                        {
-                            hs1 = lisths1[i];
-                            lisths1[i] = lisths1[j];
-                            lisths1[j] = hs1;
-                        }
-                    }
-                Console.WriteLine("{0, -7} {1, -20} {2, -10} {3, -10} {4, -15} {5,-15} {6,-10} {7,-9} {8,-10} {9,-10} {10,-12} {11,-10}",
-                             "MaHS", "TenHS", "Ngay Sinh", "Gioi Tinh", "Dia Chi", "SĐT", "Diem Thi", "Khoi thi", "Nganh", "Dien UT", "So luong NV", "Phuong thuc xet tuyen");
-                foreach (HocSinh hs in lisths1)
-                {
-                    Console.WriteLine("{0, -7} {1, -20} {2, -10} {3, -10} {4, -15} {5,-15} {6,-10} {7,-9} {8,-10} {9,-10} {10,-12} {11,-10}",
-                                   hs.MA, hs.TEN, hs.NGSINH, hs.GTINH, hs.DIACHI, hs.SDT, hs.Diemthi, hs.Khoithi, hs.Nganh, hs.DienUT, hs.SoluongNV, hs.Phuongthucxettuyen);
-                }
-            }
-        }
-    }
-} */
