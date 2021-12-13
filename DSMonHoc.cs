d0c65b02a012f54300a872d67db327e120fab8cd
YamiDP <20110546@student.hcmute.edu.vn>
2021-12-13 22:22:39 +0700

    Cap nhat class MonHoc va DSMonHoc

---
+++ DSMonHoc.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace SchoolManager
+{
+    public class DSMonHoc
+    {
+        //khoi tao danh sach cac mon hoc
+        public static List<MonHoc> listmh = new List<MonHoc>();
+        //tao du lieu danh sach mon hoc co ban
+        public static void base_list()
+        {
+            MonHoc mh1 = new MonHoc("MH1","Dai so tuyen tinh",4,"Ly thuyet");
+            listmh.Add(mh1);
+            MonHoc mh2 = new MonHoc("MH2","Toan roi rac va LTDT",3,"Ly thuyet");
+            listmh.Add(mh2);
+            MonHoc mh5 = new MonHoc("MH5","Nhap mon lap trinh",3,"Ly thuyet");
+            listmh.Add(mh5);
+            MonHoc mh3 = new MonHoc("MH3","Vat ly 1",3,"Ly thuyet");
+            listmh.Add(mh3);
+            MonHoc mh4 = new MonHoc("MH4","Thi nghiem vat ly 1",2,"Thuc hanh");
+            listmh.Add(mh4);
+            MonHoc mh6 = new MonHoc("MH6","Tu tuong HCM",3,"Ly thuyet");
+            listmh.Add(mh6);
+        }
+        //kiem tra mon hoc co ton tai trong danh sach
+        public static MonHoc checkmh(string MaMH)
+        {
+            foreach (MonHoc mh in listmh)
+            {
+                if (String.Compare(mh.Mamh, MaMH, false) == 0)
+                    return mh;
+            }
+            return null;
+        }
+        //them mon hoc vao danh sach
+        public static void addMH()
+        {
+            MonHoc mh = new MonHoc();
+            mh.inputMH();
+            listmh.Add(mh);
+            Console.WriteLine("Them mon hoc thanh cong!");
+            Console.WriteLine();
+        }
+        //in danh sach mon hoc
+        public static void printDSMH()
+        {
+            Console.WriteLine("{0, -7} {1, -20} {2,-10}{3,-15}",
+                  "Ma MH", "Ten MH","So TC","Loai mon");
+            foreach (MonHoc mh in listmh)
+            {
+                mh.print();
+            }
+            if (listmh.Count == 0)
+            {
+                Console.WriteLine("Khong co Mon Hoc de hien thi!");
+            }
+            Console.WriteLine();
+        }
+        //xoa mon hoc ra khoi danh sach
+        public static void deleteMH()
+        {
+            MonHoc monHoc;
+            do{
+                Console.Write("Nhap Ma mon hoc muon huy: ");
+                string MaMH = Console.ReadLine();
+                monHoc = checkmh(MaMH);
+                if (monHoc == null)
+                {
+                    Console.WriteLine("Khong tim thay mon hoc!");
+                    Console.WriteLine("Vui long nhap lai Ma moi!!!");
+                }
+            }while(monHoc == null);
+            listmh.Remove(monHoc);
+            Console.WriteLine("Da xoa thanh cong!");
+        }
+        //sap xep mon hoc theo ma mon hoc
+        public static void sort_MA()
+        {
+            if(listmh.Count == 0)
+            {
+                Console.WriteLine("Khong co mon hoc nao trong danh sach!");
+                return;
+            }
+            var DSMH = listmh.OrderBy(mh=>mh.Mamh);
+            Console.WriteLine("Danh sach sau khi sap xep theo ma mon hoc:");
+            Console.WriteLine("{0, -7} {1, -20} {2,-10}{3,-15}",
+                  "Ma MH", "Ten MH","So TC","Loai mon");
+            foreach (MonHoc mh in DSMH)
+            {
+                mh.print();
+            }
+            Console.WriteLine();
+        }
+        //sap xep mon hoc theo ten
+        public static void sort_name()
+        {
+            if(listmh.Count == 0)
+            {
+                Console.WriteLine("Khong co mon hoc nao trong danh sach!");
+                return;
+            }
+            var DSMH = listmh.OrderBy(mh=>mh.TenMH);
+            Console.WriteLine("Danh sach sau khi sap xep theo ten:");
+            Console.WriteLine("{0, -7} {1, -20} {2,-10}{3,-15}",
+                  "Ma MH", "Ten MH","So TC","Loai mon");
+            foreach (MonHoc mh in DSMH)
+            {
+                mh.print();
+            }
+            Console.WriteLine();
+        }
+        //Ham tra ve so luong mon hoc
+        public static int SLMH()
+        {
+            return listmh.Count;
+        }
+    }
+}
--- MonHoc.cs
+++ MonHoc.cs
@@ -1,4 +1,4 @@
-/*using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -31,6 +31,7 @@ namespace SchoolManager
             set{loaimon = value;}
             get{return loaimon;}
         }
+        // cac ham khoi tao
         public MonHoc()
         {      
         }
@@ -44,247 +45,31 @@ namespace SchoolManager
         public MonHoc(MonHoc mh)
             : this (mh.mamh,mh.tenmh, mh.sotc, mh.loaimon)
         {
-        }
 
-        public static List<MonHoc> listmh = new List<MonHoc>();
-        public static MonHoc checkmh(string MaMH)
-        {
-            foreach (MonHoc mh in listmh)
-            {
-                if (String.Compare(mh.Mamh, MaMH, false) == 0)
-                    return mh;
-            }
-            return null;
         }
-        public void inputMH(string mamh)
+        //ham huy
+        ~MonHoc()
         {
-            MonHoc mh;
-            Mamh = mamh;
+
+        }
+        //ham nhap mot mon hoc
+        public void inputMH()
+        {
+            Console.Write("Nhap ma mon hoc: ");
+            Mamh = Convert.ToString(Console.ReadLine());
             Console.Write("Ten mon hoc: ");
             TenMH = Convert.ToString(Console.ReadLine());
             Console.Write("So tin chi: ");
             SoTC = Convert.ToInt32(Console.ReadLine());
             Console.Write("Loai mon : ");
             LoaiMon = Convert.ToString(Console.ReadLine());
-            mh = new MonHoc(Mamh, TenMH, SoTC, LoaiMon);
-            listmh.Add(mh);
-            Console.WriteLine("Them Mon hoc thanh cong!");
         }
-        public static void deleteMH()
+        //ham in mot mon hoc
+        public void print()
         {
-            MonHoc monHoc;
-            do{
-                Console.Write("Nhap Ma mon hoc muon huy: ");
-                string MaMH = Console.ReadLine();
-                monHoc = checkmh(MaMH);
-                if (monHoc == null)
-                {
-                    Console.WriteLine("Khong tim thay mon hoc!");
-                    Console.WriteLine("Vui long nhap lai Ma moi!!!");
-                }
-            }while(monHoc == null);
-            listmh.Remove(monHoc);
-            Console.WriteLine("Da xoa thanh cong!");
-        }
-        public static void printMonHoc()
-        {
-            Console.WriteLine("{0, -7} {1, -20}",
-                  "MaMH", "TenMH");
-            foreach (MonHoc mh in listmh)
-            {
-                Console.WriteLine("{0, -7} {1, -20}",
-                                  mh.Mamh, mh.TenMH);
-            }
-            if (listmh.Count == 0)
-            {
-                Console.WriteLine("Khong co Mon Hoc de hien thi!");
-            }
-            Console.WriteLine();
-        }
-        public static void printMHMoLop(MonHoc mh)
-        {
-            Console.WriteLine("Mon hoc: {0}", mh.TenMH);
-            Console.WriteLine("{0, -10} {1, -20} {2, -5} {3, -10} {4, -10} {5, -20} {6, -20}",
-                  "Loai Mon", "Lop HP", "STC", "Gioi Han", "Dang Ki", "Giang Vien", "Lich Hoc");
-            foreach (DangKi dk in mh.Listdk)
-            {
-                Console.WriteLine("{0, -10} {1, -20} {2, -5} {3, -10} {4, -10} {5, -20} {6, -20}",
-                                dk.LoaiMon, dk.LopHP, dk.SoTC, dk.GioiHan, dk.SoLuongDKi, dk.TenGV, dk.LichHoc);
-            }
-            if (mh.Listdk.Count == 0)
-            {
-                Console.WriteLine("Khong co Mon hoc de hien thi!");
-            }
-            Console.WriteLine();
+                Console.WriteLine("{0, -7} {1, -20} {2,-10}{3,-15}",
+                                  this.Mamh, this.TenMH, this.SoTC, this.LoaiMon);
         }
     }
 } 
 
-/* namespace SchoolManager.QLMonHoc
-{
-    public class DangKi : MonHoc
-    {
-        private string magv;
-        private string tengv;
-        private string lophp;
-        private int gioihan;
-        private int soluongdki;
-        private string lichhoc;
-        public string MaGV
-        {
-            set{magv = value;}
-            get{return magv;}
-        }
-        public string TenGV
-        {
-            set{tengv = value;}
-            get{return tengv;}
-        }
-        public string LopHP
-        {
-            set{lophp = value;}
-            get{return lophp;}
-        }
-        public int GioiHan
-        {
-            set{gioihan = value;}
-            get{return gioihan;}
-        }
-        public int SoLuongDKi
-        {
-            set{soluongdki = value;}
-            get{return soluongdki;}
-        }
-        public string LichHoc
-        {
-            set{lichhoc = value;}
-            get{return lichhoc;}
-        }
-        public DangKi()
-        {   
-        }
-        public DangKi(string mamh, string tenmh, int sotc, string loaimon, List<DangKi> listdki, string magv, string tengv, string lophp ,int gioihan, int soluongdki,string lichhoc)
-            : base(mamh, tenmh, sotc, loaimon, listdki)
-        {
-            this.magv = magv;
-            this.tengv = tengv;
-            this.lophp = lophp;
-            this.gioihan = gioihan;
-            this.soluongdki = soluongdki;
-            this.lichhoc = lichhoc;
-        }
-        public DangKi(DangKi dk)
-            : this (dk.Mamh, dk.TenMH, dk.SoTC, dk.LoaiMon, dk.Listdk, dk.MaGV, dk.TenGV, dk.LopHP, dk.GioiHan, dk.SoLuongDKi, dk.LichHoc)
-        {
-        }
-
-        public static List<DangKi> listdk = new List<DangKi>();
-        public static DangKi checkDK(MonHoc mh, string MaGV) // check mã GV
-        {
-            foreach (DangKi dk in mh.Listdk)
-            {
-                if (String.Compare(dk.MaGV, MaGV, false) == 0)
-                    return dk;
-            }
-            return null;
-        }
-        public static DangKi checkLopHP(MonHoc mh, string LopHP) // check Lop HP
-        {
-            foreach (DangKi dk in mh.Listdk)
-            {
-                if (String.Compare(dk.LopHP, LopHP, false) == 0)
-                    return dk;
-            }
-            return null;
-        }
-        public static void DKiMoLop(MonHoc mh)
-        {
-            GiangVien gv; 
-            DangKi dk;  
-            string MaGV;    
-            do
-            {
-                Console.Write("Nhap Ma Giang Vien muon dang ki mo lop: ");
-                MaGV = Console.ReadLine();
-                gv = managerGiangVien.checkGV(MaGV);
-                if (gv == null)
-                {
-                    Console.WriteLine("Khong co Giang Vien nay trong truong!");
-                    Console.WriteLine("Vui long nhap lai Ma Giang Vien!!!");
-                }
-            } while (gv == null);
-
-            dk = checkDK(mh, MaGV);
-            if (dk != null)
-            {
-                Console.WriteLine("Giang Vien nay da dang ki mon hoc nay!");
-                return;
-            }
-            DangKi dki = new DangKi();
-            Console.Write("Lop hoc phan: ");
-            dki.lophp = Convert.ToString(Console.ReadLine());
-            Console.Write("Gioi han: ");
-            dki.gioihan = Convert.ToInt32(Console.ReadLine());
-            dki.soluongdki = 0;
-            Console.Write("Lich hoc: ");
-            dki.lichhoc = Convert.ToString(Console.ReadLine());
-            dk = new DangKi(mh.Mamh, mh.TenMH, mh.SoTC, mh.LoaiMon, mh.Listdk, gv.MA, gv.TEN, dki.lophp, dki.gioihan, dki.soluongdki, dki.lichhoc);
-            mh.Listdk.Add(dk);
-            Console.WriteLine("Dang ki mo lop thanh cong!!!");
-        }
-        public static void DKiMonHoc(MonHoc mh)
-        {
-            SinhVien sv;
-            string MaSV;
-            DangKi dangKi; 
-            string LopHP;    
-
-            if(mh.Listdk.Count == 0)
-            {
-                Console.WriteLine("Khong co Giang Vien nao dang ki mo lop mon nay!");
-                return;
-            }
-            do
-            {
-                Console.Write("Nhap Ma Sinh Vien muon dang ki mon hoc: ");
-                MaSV = Console.ReadLine();
-                sv = managerSinhVien.checkSV(MaSV);
-                if (sv == null)
-                {
-                    Console.WriteLine("Khong co Sinh Vien nay trong truong!");
-                    Console.WriteLine("Vui long nhap lai Ma Sinh Vien!!!");
-                }
-            } while (sv == null);
-
-            managerMonHoc.printMHMoLop(mh);
-
-            do
-            {
-                Console.Write("Chon Lop hoc phan: ");
-                LopHP = Console.ReadLine();
-                dangKi = checkLopHP(mh, LopHP);
-                if (dangKi == null)
-                {
-                    Console.WriteLine("Khong co Lop hoc phan nay!");
-                    Console.WriteLine("Vui long nhap lai Lop hoc phan!!!");
-                }
-            } while (dangKi == null);
-
-            foreach (DangKi dk in mh.Listdk)
-            {
-                if (String.Compare(dk.LopHP, dangKi.LopHP, false) == 0)
-                {
-                    if(dk.SoLuongDKi == dk.GioiHan)
-                    {
-                        Console.WriteLine("Lop hoc phan nay da du so luong!!!");
-                        return;
-                    }
-                    dk.SoLuongDKi++;
-                }
-            }
-            // bị trùng mã lớp hp vẫn đki đc
-            //sv.ListmhSV.Add(mh);
-            Console.WriteLine("Dang ki Mon Hoc thanh cong!!!");
-        }
-    }
-} */
